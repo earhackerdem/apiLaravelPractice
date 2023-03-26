2ee026b10e62e7514cfc85d4127b2f0cd705ef3e
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2023-03-26 17:53:21 -0600

    Version 1 - Colección

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class PostController extends Controller
 {
@@ -14,15 +15,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+        return PostResource::collection(Post::latest()->paginate());
     }
 
     /**
@@ -34,18 +27,15 @@ class PostController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Post $post)
-    {
-        //
-    }
-
-    /**
      * Remove the specified resource from storage.
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return response()->json([
+            'message' => 'Post Deleted'
+        ], Response::HTTP_NO_CONTENT);
     }
+
+    //https://medium.com/@naumancs/how-to-use-http-status-codes-properly-in-laravel-3f66eebf0e66
 }
--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('v1/posts',App\Http\Controllers\Api\V1\PostController::class)->only('show');
+Route::apiResource('v1/posts',App\Http\Controllers\Api\V1\PostController::class)
+->only(['index','show']);
