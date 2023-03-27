337bc7fc3d30bb334338a7e9db0a74b3d3c4f2e6
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2023-03-26 21:47:16 -0600

    Version 2: recurso

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -17,5 +18,10 @@ class Post extends Model
     public function getPublishedAtAttribute()
     {
         return $this->created_at->format('d/m/Y');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
