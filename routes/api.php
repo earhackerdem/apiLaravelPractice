e91acd04948776ec116c302242ff93f21c0f1bc4
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2023-03-26 21:25:46 -0600

    Versión 2: planificación y configuración inicial

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('v1/posts',App\Http\Controllers\Api\V1\PostController::class)
+Route::apiResource('v1/posts',PostV1::class)
+->only(['index','show','destroy']);
+
+Route::apiResource('v2/posts',PostV2::class)
 ->only(['index','show','destroy']);
