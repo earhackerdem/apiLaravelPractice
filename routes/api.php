74ad89072de6ca34de60befdf3c0e75d48ef0d7a
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2023-03-26 22:51:34 -0600

    Autenticación parte 1

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\PostController as PostV1;
@@ -9,10 +10,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('v1/posts',PostV1::class)
-->only(['index','show','destroy'])
-->middleware('auth:sanctum');
+Route::apiResource('v1/posts', PostV1::class)
+    ->only(['index', 'show', 'destroy'])
+    ->middleware('auth:sanctum');
 
-Route::apiResource('v2/posts',PostV2::class)
-->only(['index','show','destroy'])
-->middleware('auth:sanctum');
+Route::apiResource('v2/posts', PostV2::class)
+    ->only(['index', 'show', 'destroy'])
+    ->middleware('auth:sanctum');
+
+Route::post('login', [
+    LoginController::class,
+    'login'
+]);
